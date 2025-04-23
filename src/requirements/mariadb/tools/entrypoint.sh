b40baa3a2e4da83435e260e075d7123faee90fa0
Phroms <hernangrim.10@gmail.com>
2025-04-23 08:07:03 -0500

    Nginx working, problem with mariadb

--- src/requirements/mariadb/tools/entrypoint.sh
+++ src/requirements/mariadb/tools/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
-envsubst < /docker-entrypoint-initdb.d/init-db.template > /docker-entrypoint-initdb.d/init-db.sql
+# Solo ejecutar init si la base de datos aún no fue inicializada
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "⏳ Inicializando base de datos..."
+    envsubst < /docker-entrypoint-initdb.d/init-db.template > /docker-entrypoint-initdb.d/init-db.sql
+    mysqld --user=mysql --bootstrap < /docker-entrypoint-initdb.d/init-db.sql
+else
+    echo "✅ Base de datos ya inicializada. Saltando creación."
+fi
 
-exec mysqld_safe --init-file=/docker-entrypoint-initdb.d/init-db.sql
+exec mysqld_safe
